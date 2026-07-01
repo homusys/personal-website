d1183f6f5ca20a347d264c904c35d54af7e4d301
homusys <gitchewycide@gmail.com>
2026-07-01 19:33:32 +0800

    Add Theme interface
    The Theme inteface is for implementing the light and dark themes of the
    webpage

---
+++ src/core/theme.js
@@ -0,0 +1,16 @@
+
+/**
+ * The Theme class is an interface used for implementing Dark and Light
+ * themes of the webpage and possibly for creating a customized theme.
+ */
+export class Theme {
+    changeIcon() {};
+}
+
+
+export class LightTheme extends Theme {
+}
+
+
+export class DarkTheme extends Theme {
+}
