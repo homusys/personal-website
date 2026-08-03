e19c6fc96e58a797a0ae57f5be1e37a7dc210040
homusys <gitchewycide@gmail.com>
2026-08-03 19:03:05 +0800

    Add project and certification page links and refactor code
    A diagonal arrow icon is added to the links to emphasize that the user
    will be going to that page on click

--- src/index.js
+++ src/index.js
@@ -4,141 +4,136 @@ import { goldenAppleImage } from "./assets/images/images.js";
 import profilePic from "./assets/images/profile.png";
 import { default as resumePDF } from "./Carl_Matthew_Arzadon_Resume.pdf";
 
-
 /**
  * The DOMHandler class is responsible for all the DOM operations.
  */
 class DomHandler {
-    /**
-     * 
-     * @param {*} buttonId A CSS selector string of the target button's id attribute.
-     * @param {*} pathD The svg path's d attribute.
-     */
-    addSvgIconToButton(buttonId, pathD, {size}) {
-        const VIEWBOX = "0 0 24 24";
+  /**
+   *
+   * @param {*} buttonId A CSS selector string of the target button's id attribute.
+   * @param {*} pathD The svg path's d attribute.
+   */
+  addSvgIconToButton(buttonId, pathD, { size }) {
+    const VIEWBOX = "0 0 24 24";
 
-        const button = document.querySelector(buttonId);
-        if (!button) {
-            return;
-        }
-        button.classList.add("icon__button--size-l");
+    const button = document.querySelector(buttonId);
+    if (!button) {
+      return;
+    }
+    button.classList.add("icon__button--size-l");
 
-        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-        svg.setAttribute("viewBox", VIEWBOX);
-        svg.classList.add("icon");
-        this.setSvgIconSize(svg, size);
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", VIEWBOX);
+    svg.classList.add("icon");
+    this.setSvgIconSize(svg, size);
 
-        const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute("d", pathD);
-        
-        svg.appendChild(path);
-        button.appendChild(svg);
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    path.setAttribute("d", pathD);
+
+    svg.appendChild(path);
+    button.appendChild(svg);
+  }
+
+  setSvgIconSize(svgElement, size = "s") {
+    switch (size) {
+      case "s":
+        svgElement.classList.add("icon--size-s");
+        break;
+      case "m":
+        svgElement.classList.add("icon--size-m");
+        break;
+      case "l":
+        svgElement.classList.add("icon--size-l");
+        break;
+    }
+  }
+
+  collectionBuilder() {
+    const stack = new Set();
+    const add = (item) => {
+      if (typeof item !== "string") {
+        throw new Error(
+          "The item being added to the tech stack is not a String.",
+        );
+      }
+      stack.add(item);
+    };
+    const getStack = () => {
+      return stack;
+    };
+
+    return { add, getStack };
+  }
+
+  addToTechStackCollection(targetId, ...stackItems) {
+    const collection = document.querySelector(targetId);
+
+    for (const item of stackItems[0]) {
+      const stackItem = document.createElement("span");
+      stackItem.classList.add("stack__item");
+      stackItem.textContent = item;
+      collection.appendChild(stackItem);
+    }
+  }
+
+  createExpCard(imageSrc, title, company, startEndDate, ...stackItems) {
+    const IMG_PLACEHOLD =
+      "https://placehold.co/600x400/000000/fff?text=this+is+a+placeholder+image";
+
+    const expColl = document.querySelector("#expColl");
+
+    const expCard = document.createElement("div");
+    expCard.classList.add("exp__card");
+
+    const cardImage = document.createElement("figure");
+    cardImage.classList.add("card__image");
+
+    const img = document.createElement("img");
+    img.src = !imageSrc ? IMG_PLACEHOLD : imageSrc;
+
+    const cardDetails = document.createElement("div");
+    cardDetails.classList.add("card__details");
+
+    const header = document.createElement("h3");
+    header.textContent = title;
+    header.classList.add("font-header");
+
+    const companyP = document.createElement("p");
+    companyP.textContent = company;
+
+    const startEndDateP = document.createElement("p");
+    startEndDateP.textContent = startEndDate;
+
+    const techUsed = document.createElement("div");
+    techUsed.classList.add("tech__used");
+
+    for (const item of stackItems[0]) {
+      const stackItem = document.createElement("span");
+      stackItem.classList.add("stack__item");
+      stackItem.textContent = item;
+      techUsed.appendChild(stackItem);
     }
 
-    
-    setSvgIconSize(svgElement, size="s") {
-        switch (size) {
-            case "s":
-                svgElement.classList.add("icon--size-s");
-                break;
-            case "m":
-                svgElement.classList.add("icon--size-m");
-                break;
-            case "l":
-                svgElement.classList.add("icon--size-l");
-                break;
-        }
-    }
+    const link = document.createElement("a");
+    link.classList.add("card__link");
+    link.href = "";
+    link.textContent = "Click for more details";
 
+    cardImage.appendChild(img);
 
-    collectionBuilder() {
-        const stack = new Set();
-        const add = (item) => {
-            if (typeof item !== "string") {
-                throw new Error(
-                "The item being added to the tech stack is not a String.",
-                );
-            }
-            stack.add(item);
-        }
-        const getStack = () => {
-            return stack;
-        }
+    cardDetails.appendChild(header);
+    cardDetails.appendChild(companyP);
+    cardDetails.appendChild(startEndDateP);
 
-        return { add, getStack };
-    }
+    expCard.appendChild(cardImage);
+    expCard.appendChild(cardDetails);
+    expCard.appendChild(techUsed);
+    expCard.appendChild(link);
 
+    expColl.appendChild(expCard);
+  }
 
-    addToTechStackCollection(targetId, ...stackItems) {
-        const collection = document.querySelector(targetId);
-
-        for (const item of stackItems[0]) {
-            const stackItem = document.createElement("span");
-            stackItem.classList.add("stack__item");
-            stackItem.textContent = item;
-            collection.appendChild(stackItem);
-        }
-        
-    }
-
-
-    createExpCard(imageSrc, title, company, startEndDate, ...stackItems) {
-        const IMG_PLACEHOLD = "https://placehold.co/600x400/000000/fff?text=this+is+a+placeholder+image";
-
-        const expColl = document.querySelector("#expColl");
-        
-        const expCard = document.createElement("div");
-        expCard.classList.add("exp__card");
-        
-        const cardImage = document.createElement("figure");
-        cardImage.classList.add("card__image")
-        
-        const img = document.createElement("img");
-        img.src = (!imageSrc) ? IMG_PLACEHOLD : imageSrc;
-
-        const cardDetails = document.createElement("div");
-        cardDetails.classList.add("card__details");
-        
-        const header = document.createElement("h3");
-        header.textContent = title;
-        header.classList.add("font-header");
-
-        const companyP = document.createElement("p");
-        companyP.textContent = company;
-
-        const startEndDateP = document.createElement("p");
-        startEndDateP.textContent = startEndDate;
-
-        const techUsed = document.createElement("div");
-        techUsed.classList.add("tech__used");
-
-        for (const item of stackItems[0]) {
-            const stackItem = document.createElement("span");
-            stackItem.classList.add("stack__item");
-            stackItem.textContent = item;
-            techUsed.appendChild(stackItem);
-        }
-
-        const link = document.createElement("a");
-        link.classList.add("card__link");
-        link.href = "";
-        link.textContent = "Click for more details";
-
-        cardImage.appendChild(img);
-
-        cardDetails.appendChild(header);
-        cardDetails.appendChild(companyP);
-        cardDetails.appendChild(startEndDateP);
-        
-        expCard.appendChild(cardImage);
-        expCard.appendChild(cardDetails);
-        expCard.appendChild(techUsed);
-        expCard.appendChild(link);
-
-        expColl.appendChild(expCard);
-    }
-
-    // TODO: Create a setter for switching themes (dark and light theme).
+  // TODO: Create a setter for switching themes (dark and light theme).
 }
 
 const dom = new DomHandler();
@@ -173,67 +168,78 @@ exp1.add("MySQL");
 exp1.add("Git");
 exp1.add("GitHub");
 dom.createExpCard(
-    null,
-    "Web Developer Intern",
-    "Department of the Interior and Local Government - Cordillera Administrative Region",
-    "June 2025 - August 2025",
-    exp1.getStack()
+  null,
+  "Web Developer Intern",
+  "Department of the Interior and Local Government - Cordillera Administrative Region",
+  "June 2025 - August 2025",
+  exp1.getStack(),
 );
 
 // Initialize icons to HTML
 const iconsHandler = new IconHandler();
-iconsHandler.initIcons()
-    .then(( iconPaths ) => {
-        const defaultIconConfig = {
-            "size": "m"
-        }
+iconsHandler
+  .initIcons()
+  .then((iconPaths) => {
+    const defaultIconConfig = {
+      size: "m",
+    };
 
-        const heroIconsConfig = {
-            "size": "l"
-        }
+    const heroIconsConfig = {
+      size: "l",
+    };
 
-        dom.addSvgIconToButton(
-            "#navBurger",
-            iconsHandler.getIconPathDFromId("burger"),
-            defaultIconConfig
-        );
+    dom.addSvgIconToButton(
+      "#navBurger",
+      iconsHandler.getIconPathDFromId("burger"),
+      defaultIconConfig,
+    );
 
-        dom.addSvgIconToButton(
-            "#navThemer",
-            iconsHandler.getIconPathDFromId("moon"),
-            defaultIconConfig
-        );
+    dom.addSvgIconToButton(
+      "#navThemer",
+      iconsHandler.getIconPathDFromId("moon"),
+      defaultIconConfig,
+    );
 
-        dom.addSvgIconToButton(
-            "#resumeLink",
-            iconsHandler.getIconPathDFromId("download"),
-            heroIconsConfig
-        );
+    dom.addSvgIconToButton(
+      "#resumeLink",
+      iconsHandler.getIconPathDFromId("download"),
+      heroIconsConfig,
+    );
 
-        dom.addSvgIconToButton(
-            "#githubLink",
-            iconsHandler.getIconPathDFromId("github"),
-            heroIconsConfig
-        );
+    dom.addSvgIconToButton(
+      "#githubLink",
+      iconsHandler.getIconPathDFromId("github"),
+      heroIconsConfig,
+    );
 
-        dom.addSvgIconToButton(
-            "#linkedInLink",
-            iconsHandler.getIconPathDFromId("linkedin"),
-            heroIconsConfig
-        );
+    dom.addSvgIconToButton(
+      "#linkedInLink",
+      iconsHandler.getIconPathDFromId("linkedin"),
+      heroIconsConfig,
+    );
 
-        dom.addSvgIconToButton(
-            "#emailLink",
-            iconsHandler.getIconPathDFromId("email"),
-            heroIconsConfig
-        );
-    })
-    .then(() => {
-        const heroProfilePic = document.querySelector("#profilePic");
-        heroProfilePic.setAttribute("src", profilePic);
+    dom.addSvgIconToButton(
+      "#emailLink",
+      iconsHandler.getIconPathDFromId("email"),
+      heroIconsConfig,
+    );
 
-        const resumeLink = document.querySelector("#resumeLink");
-        resumeLink.setAttribute("href", resumePDF);
-    });
+    dom.addSvgIconToButton(
+      "#certificationsLink",
+      iconsHandler.getIconPathDFromId("arrow"),
+      { size: "m" },
+    );
 
-    
+    dom.addSvgIconToButton(
+      "#projectsLink",
+      iconsHandler.getIconPathDFromId("arrow"),
+      { size: "m" },
+    );
+  })
+  .then(() => {
+    const heroProfilePic = document.querySelector("#profilePic");
+    heroProfilePic.setAttribute("src", profilePic);
+
+    const resumeLink = document.querySelector("#resumeLink");
+    resumeLink.setAttribute("href", resumePDF);
+  });
--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,48 @@ import { fileURLToPath } from "node:url";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default {
-    entry: "./src/index.js",
-    mode: "development",
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      filename: "pages/projects.html",
+      template: "./src/pages/projects.html",
+    }),
+  ],
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    watchFiles: ["./src/index.html"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.pdf$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "[name].[contenthash][ext]",
+        },
+      },
     ],
-    devtool: "eval-source-map",
-    devServer: {
-        static: "./dist",
-        watchFiles: ["./src/index.html"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif|pdf)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.pdf$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: "[name].[contenthash][ext]",
-                }
-            },
-        ],
-    },
+  },
 };
