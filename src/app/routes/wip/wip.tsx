2cd0e34dd0848bab9e825e8762bf79b89adb1839
homusys <gitchewycide@gmail.com>
2026-08-19 15:50:50 +0800

    Add styling and transition animation to wip page

--- src/app/routes/wip/wip.tsx
+++ src/app/routes/wip/wip.tsx
@@ -1,14 +1,20 @@
 import { NavLink } from "react-router";
+import Curtains from "../../components/curtains/curtains";
+
+import "./wip.css";
 
 export default function WIP() {
   return (
-    <div id="wip">
-      <span>
-        <h3 className="font-header">Currently in development 🚧</h3>
-      </span>
-      <NavLink to="/" className="link--strong-true">
-        Go back
-      </NavLink>
-    </div>
+    <>
+      <Curtains />
+      <div id="wip">
+        <span>
+          <h3 className="font-header">Currently in development 🚧</h3>
+        </span>
+        <NavLink to="/" className="link--strong-true">
+          Go back
+        </NavLink>
+      </div>
+    </>
   );
 }
